f0e01abce34b2d8c80dc338061eca128cc78cc79
Andrés Torres <towerspro@gmail.com>
2020-05-25 18:12:39 +0100

    first commit

---
+++ src/main/java/com/backbase/PizzaResourceDoneable.java
@@ -0,0 +1,12 @@
+package com.backbase;
+
+import io.fabric8.kubernetes.api.builder.Function;
+import io.fabric8.kubernetes.client.CustomResourceDoneable;
+
+public class PizzaResourceDoneable extends CustomResourceDoneable<PizzaResource> {
+
+    public PizzaResourceDoneable(PizzaResource resource, Function<PizzaResource, PizzaResource> function) {
+        super(resource, function);
+    }
+}
+
---
+++ src/main/java/com/backbase/PizzaResourceList.java
@@ -0,0 +1,10 @@
+package com.backbase;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import io.fabric8.kubernetes.client.CustomResourceList;
+
+@JsonSerialize
+public class PizzaResourceList extends CustomResourceList<PizzaResource> {
+
+}
+
